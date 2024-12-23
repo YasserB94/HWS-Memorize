de67dd1e3652cd3ab574492711f9b2d57c317156
Yasser Bal <yasserbal@icloud.com>
2024-12-23 10:38:26 +0100

    MemoryItem:Identifiable

--- Memorize/MemoryItem.swift
+++ Memorize/MemoryItem.swift
@@ -4,8 +4,15 @@
 //
 //  Created by Yasser Bal on 23/12/2024.
 //
+import Foundation
 
-struct MemoryItem:Codable {
+struct MemoryItem:Codable,Identifiable {
+    let id = UUID()
     let title:String
     let text:String
+    
+    enum CodingKeys:String, CodingKey {
+        case title
+        case text
+    }
 }
