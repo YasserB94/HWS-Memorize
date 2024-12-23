10620291412b2d1ab5925e01233055c28d5b399d
Yasser Bal <yasserbal@icloud.com>
2024-12-23 10:38:55 +0100

    ContentViewModel - [MemoryItem] loading

--- Memorize/ContentViewModel.swift
+++ Memorize/ContentViewModel.swift
@@ -8,5 +8,31 @@
 import Foundation
 
 class ContentViewModel:ObservableObject {
-    @Published var message:String = "Hi Mom!"
+    @Published var items:[MemoryItem]
+    
+    init(){
+        self.items = Self.loadMemoryItems()
+    }
+    
+    private static func loadMemoryItems() -> [MemoryItem] {
+        guard let url = Bundle.main.url(forResource: "MemoryItems", withExtension: "json") else {
+            fatalError("Failed to locate MemoryItems.json in bundle")
+        }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            return try decoder.decode([MemoryItem].self, from: data)
+        } catch is CocoaError {
+            print("Failed to read data")
+//            fatalError("Failed to read data from \(url.path())")
+        } catch is DecodingError {
+            print("Failed to decode")
+//            fatalError("Failed to decode json data from \(url.path())")
+        } catch {
+            print("other err")
+//            fatalError("Unknown error loading memoryItems \(error.localizedDescription)")
+        }
+        return []
+    }
 }
