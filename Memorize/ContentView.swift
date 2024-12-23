5ac8f54ae0cda4ad16d74745a679fe98f0aaa685
Yasser Bal <yasserbal@icloud.com>
2024-12-23 10:14:20 +0100

    ContentViewModel

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var model = ContentViewModel()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(model.message)
         }
         .padding()
     }
---
+++ Memorize/ContentViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentViewModel.swift
+//  Memorize
+//
+//  Created by Yasser Bal on 23/12/2024.
+//
+
+import Foundation
+
+class ContentViewModel:ObservableObject {
+    @Published var message:String = "Hi Mom!"
+}
